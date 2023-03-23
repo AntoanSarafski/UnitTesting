e4534fd07c98faff27e21aeaeeb2094e44ee5103
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-23 20:54:12 +0200

    Add project files.

---
+++ Car.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Car.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(6, 6);
+        }
+    }
+}
---
+++ UnitTesting/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    public class Car
+    {
+        public void Drive(int kms)
+        {
+            if (kms < 0 )
+            {
+                throw new ArgumentException("Don't Lie!");
+            }
+            Mileage += kms;
+        }
+
+        public int Mileage { get; set; }
+    }
+}
