ac0bf10c1f2e1a971824bc51969d19c71dce486b
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-23 21:18:04 +0200

    Commit with correct way to use AreEqual.

--- Car.Test/CarTests.cs
+++ Car.Test/CarTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Cars;
+using System;
 
 namespace Cars.Test
 {
@@ -12,7 +13,20 @@ namespace Cars.Test
 
             car.Drive(100);
 
-            Assert.AreEqual(car.Mileage, 100);
+            Assert.AreEqual(100, car.Mileage); // First expected value - 100, then checked value.
+        }
+
+        [Test]
+        public void When_Car_Is_Drive_Mileage_Should_Throw_Error_With_Negative_kms() // or Test_Drive_Car_Should_Increase_Mileage()
+        {
+            Car car = new Car();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                car.Drive(-100);
+            });
+            
+
         }
     }
 }
